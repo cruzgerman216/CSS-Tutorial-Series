64437116a03325a3dd475e37b21601dded8f851e
= <cruzgerman216@yahoo.com>
2019-05-04 22:49:25 -0700

    new file:   ../../Javascript/part1statements/intro.js

---
+++ Javascript/part1statements/intro.js
@@ -0,0 +1,13 @@
+//ydkj
+
+//code -> source code, saved in text file, syntax,
+// statements
+
+var b = 2;
+var a = b * 2;
+console.log(a);
+
+//if c does not contain a value then the result is NaN
+var c;
+a = c * 2;
+console.log(a);
