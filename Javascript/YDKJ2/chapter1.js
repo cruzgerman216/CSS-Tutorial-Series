fa3390294f0e073f28c44cba8533cbcdd29c1852
= <cruzgerman216@yahoo.com>
2019-05-16 14:10:50 -0700

    new file:   CSS/navbar/index.css 	new file:   CSS/navbar/index.html 	modified:   Javascript/YDKJ2/chapter1.js

--- Javascript/YDKJ2/chapter1.js
+++ Javascript/YDKJ2/chapter1.js
@@ -24,5 +24,43 @@
 
 //Code-Generation
 //The process of taking an AST and turning it into executable code
-//in a simple way, the AST for var a = 2; is turned into machine instructions to actually create a variable called a (including reserving memory) and then store a value in a
-//
+//in a simple way, the AST for var a = 2; is turned into machine instructions to actually create a variable called a (including reserving memory)
+//and then store a value in a
+//JS has to be compiiled before it's executed (simplicity)
+//NOTE: Compiler works like this but this is simplicity terms.soemthing that's not worth going over but worth mentioning i guess
+
+//Understanding Scope
+//var a = 2;
+// engine  - responsible for start to finish compilation and execution of our javascript progarm
+// compiler - one of engine's friends; handles all the dirty work of parsing and code-generation
+// scope: another friend of engine; collects and maintains a look-up list of all decalred identifers(variables) and
+//enforces a strict
+// set of rules as to how these are accessible to current executing code
+
+//NOTE: get's pretty technical at this point.....
+
+//The first thing Compiler will do with this program is perform lexing to break it down into tokens, which it will then
+//parse into a tree. But
+//when Compiler gets to code generation, it will treat this program somewhat differently than perhaps assumed
+
+//first the compiler decalres a variable in the current scope and second, when executing, engine looks up the varaible
+//in scope and assigns it if found.
+
+//Compiler Speak
+//When the engine executes the code compiled produced for step 2 it looks up the variable a to see if it has been declared
+//this look-up engine performs affects the outcome of the look-up
+//look-up LHS and RHS (lefthand righthand) of an assignment operation
+//LHS is when a variable appears on the lefthand side of an assigment operation and the RHS appears on the righthand side
+//of an assignment operation
+//"who's the target of the assignment(LHS) and "who's the source of the assignment(RHS)"
+
+//engine/scope conversion
+// engine and scope communicate with one another, the engine asks scope if scope knows LHS or RHS of the variables
+//scope will know or not know
+
+//nested scopes
+//scopes are sometimes inside other scopes
+//when a variable cannot be found in the scope, the engine will go fourth trough the outter scopes until the outmost a.k.a
+//global scope is reach
+
+//ERRORS
